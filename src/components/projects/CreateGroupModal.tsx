6a0e7b72384c026d2b2ede47aea6f12976955b0f
Ali-Tamerr <extelx66@gmail.com>
2026-02-20 15:26:20 +0200

    remove the console.log statement from  src/components/projects/CreateGroupModal.tsx

--- src/components/projects/CreateGroupModal.tsx
+++ src/components/projects/CreateGroupModal.tsx
@@ -33,7 +33,6 @@ export function CreateGroupModal({ isOpen, onClose, onSubmit, loading, available
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isOpen]);
 
-    console.log('[Modal] initialData.projectIds:', initialData?.projectIds, 'selectedProjects:', Array.from(selectedProjects), 'availableProject IDs:', availableProjects.map(p => ({ id: p.id, type: typeof p.id })));
 
     if (!isOpen) return null;
 
